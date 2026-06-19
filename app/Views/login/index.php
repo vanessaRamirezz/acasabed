13626514a616827ef0cb455caf2fb15ad2948081
Vanessa <vaneramirezz02@gmail.com>
2026-06-19 00:15:12 -0600

    se agrego icono en le login

--- app/Views/login/index.php
+++ app/Views/login/index.php
@@ -5,6 +5,7 @@
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <title>ACASABED | Iniciar</title>
+    <link rel="icon" type="image/png" href="<?= base_url('dist/img/agua.png') ?>">
     <!-- Tell the browser to be responsive to screen width -->
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
