af28530f852ae3b1253dfcbb3fa7b58414398b22
Vanessa <vaneramirezz02@gmail.com>
2026-06-18 18:33:09 -0600

    se agrego icono de acasabed, para que aparezca en el navegador

--- app/Views/layouts/main.php
+++ app/Views/layouts/main.php
@@ -5,6 +5,8 @@
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <title><?= $this->renderSection('title') ?></title>
+    <link rel="icon" type="image/png" href="<?= base_url('dist/img/agua.png') ?>">
+
     <!-- <title>AdminLTE 3 | Dashboard</title> -->
     <!-- Tell the browser to be responsive to screen width -->
     <meta name="viewport" content="width=device-width, initial-scale=1">
